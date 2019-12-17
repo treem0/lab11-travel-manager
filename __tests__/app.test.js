e15517799dd873ae44029733b0c88fede2e6eea8
Travis Molter <travismolter@gmail.com>
2019-12-16 17:57:33 -0800

    trying to get the get all route working and i messed something up bad...

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,9 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Destination = require('../lib/models/Destination');
+const Itinerary = require('../lib/models/Itinerary');
 
+let itinerary;
 let vacation;
 
 describe('app routes', () => {
@@ -17,15 +19,21 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
-  beforeEach(() =>{
+  beforeEach(async() =>{
     vacation = 
-    Destination
+    await Destination
       .create({
         name: 'Cool Durban Trip',
         cityName: 'Durban',
         year: 2019,
         month: 12,
         day: 16
+      });
+    itinerary = await Itinerary
+      .create({
+        thingToDo: 'Sleep',
+        priority: 1,
+        destination: vacation._id
       });
   });
 
@@ -62,6 +70,30 @@ describe('app routes', () => {
           }],
           __v: 0
         });
+      });
+  });
+  it('gets all trips', () => {
+    return request(app)
+      .get('/api/v1/vacations')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.any(String),
+          name: 'Cool Durban Trip',
+          cityName: 'Durban',
+          year: 2019,
+          month: 12,
+          day: 16,
+          weather: [{
+            '_id': expect.any(String),
+            'applicable_date': '2019-12-16',
+            'humidity': 63,
+            'max_temp': 5.23,
+            'min_temp': -3.5,
+            'weather_state_name': 'Light Cloud',
+            'wind_direction_compass': 'SSW'
+          }],
+          __v: 0
+        }]);
       });
   });
 });
