9a768b66e2718acfee7d1cce92333eff1abc65b4
Travis Molter <travismolter@gmail.com>
2019-12-16 16:41:26 -0800

    got the create a trip working and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Destination = require('../lib/models/Destination');
+
+let vacation;
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,7 +17,51 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  beforeEach(() =>{
+    vacation = 
+    Destination
+      .create({
+        name: 'Cool Durban Trip',
+        cityName: 'Durban',
+        year: 2019,
+        month: 12,
+        day: 16
+      });
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('creates a trip', () =>{ 
+    return request(app)
+      .post('/api/v1/vacations')
+      .send({ 
+        name: 'Cool Durban Trip',
+        cityName: 'Durban',
+        year: 2019,
+        month: 12,
+        day: 16
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Cool Durban Trip',
+          cityName: 'Durban',
+          year: 2019,
+          month: 12,
+          day: 16,
+          weather: [{
+            '_id': expect.any(String),
+            'applicable_date': '2019-12-16',
+            'humidity': 63,
+            'max_temp': 5.23,
+            'min_temp': -3.5,
+            'weather_state_name': 'Light Cloud',
+            'wind_direction_compass': 'SSW'
+          }],
+          __v: 0
+        });
+      });
   });
 });
